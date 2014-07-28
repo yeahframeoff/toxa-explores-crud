067664cbe54b251bb79a89c5d08279f907bf23e2
yeahframeoff <anthyef@gmail.com>
2014-07-28 06:22:48 +0300

    added one more migration and changed db config

--- app/database/migrations/2014_07_27_224614_create-post-and-user.php
+++ app/database/migrations/2014_07_27_224614_create-post-and-user.php
@@ -11,9 +11,32 @@ class CreatePostAndUser extends Migration {
 	 * @return void
 	 */
 	public function up()
-	{
-		//
-	}
+    {
+        DB::transaction(function(){
+
+            if (!Schema::hasTable('users'))
+                Schema::create('users', function($table)
+                {
+                    $table->increments('id');
+
+                    $table->string('first_name', 20)->nullable();
+                    $table->string('last_name', 20)->nullable();
+                    $table->string('login', 64);
+                    $table->string('password', 64);
+                });
+
+            if (!Schema::hasTable('posts'))
+                Schema::create('posts', function($table)
+                {
+                    $table->increments('id');
+
+                    $table->string('title', 256);
+                    $table->text('content');
+                    $table->integer('user_id');
+                    $table->timestamps();
+                });
+        });
+    }
 
 	/**
 	 * Reverse the migrations.
@@ -22,7 +45,10 @@ class CreatePostAndUser extends Migration {
 	 */
 	public function down()
 	{
-		//
-	}
+        if (Schema::hasTable('posts'))
+            Schema::drop('posts');
+        if (Schema::hasTable('users'))
+            Schema::drop('users');
+    }
 
 }
